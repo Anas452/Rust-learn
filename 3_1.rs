37a79aafc4eb7069da5f1788e5e948fd747ef12c
Anas452 <anasm9877@gmail.com>
2019-08-04 00:52:14 +0500

    ed

--- 3_1.rs
+++ 3_1.rs
@@ -4,3 +4,14 @@ fn main() {
     x = 6;
     println!("The value of x is: {}", x);
 }
+
+fn main() {
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+}
+
