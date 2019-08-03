b7118198c60e0816a5afdca8c99269854d49b214
Muhammad Anas <Anas452>
2019-08-03 18:09:04 +0500

    quiz 5

---
+++ 10_1.rs
@@ -0,0 +1,180 @@
+// fn largest_i32(list: &[i32]) -> i32 {
+//     let mut largest = list[0];
+
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// fn largest_char(list: &[char]) -> char {
+//     let mut largest = list[0];
+
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest_i32(&number_list);
+//     println!("The largest number is {}", result);
+//    assert_eq!(result, 100);
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest_char(&char_list);
+//     println!("The largest char is {}", result);
+//    assert_eq!(result, 'y');
+// }
+
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest(&char_list);
+//     println!("The largest char is {}", result);
+// }
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// fn main() {
+//     let integer = Point { x: 5, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+// }
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// fn main() {
+//     let wont_work = Point { x: 5, y: 4.0 };
+// }
+
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+
+// fn main() {
+//     let both_integer = Point { x: 5, y: 10 };
+//     let both_float = Point { x: 1.0, y: 4.0 };
+//     let integer_and_float = Point { x: 5, y: 4.0 };
+// }
+
+
+// #![allow(unused_variables)]
+// fn main() {
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
+// }
+
+
+// #![allow(unused_variables)]
+// fn main() {
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+// }
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.y
+//     }
+// }
+
+// fn main() {
+//     let p = Point { x: 5, y: 10 };
+
+//     println!("p.x = {}", p.x());
+// }
+
+
+// #![allow(unused_variables)]
+// fn main() {
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// impl Point<f32> {
+//     fn distance_from_origin(&self) -> f32 {
+//         (self.x.powi(2) + self.y.powi(2)).sqrt()
+//     }
+// }
+// }
+
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+
+// impl<T, U> Point<T, U> {
+//     fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+//         Point {
+//             x: self.x,
+//             y: other.y,
+//         }
+//     }
+// }
+
+// fn main() {
+//     let p1 = Point { x: 5, y: 10.4 };
+//     let p2 = Point { x: "Hello", y: 'c'};
+
+//     let p3 = p1.mixup(p2);
+
+//     println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+// }
+
+
+enum Option_i32 {
+    Some(i32),
+    None,
+}
+
+enum Option_f64 {
+    Some(f64),
+    None,
+}
+
+fn main() {
+    let integer = Option_i32::Some(5);
+    let float = Option_f64::Some(5.0);
+}
